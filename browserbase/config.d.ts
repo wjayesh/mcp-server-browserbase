6b7e95c4dd7665af3c126bb3ac6aab34957344e9
Kylejeong2 <kylejeong21@gmail.com>
2025-05-05 17:44:56 -0700

    setup sse

---
+++ browserbase/config.d.ts
@@ -0,0 +1,30 @@
+export type Config = {
+    browserbaseApiKey?: string;
+    browserbaseProjectId?: string;
+    /** 
+     * Whether or not to use Browserbase proxies  
+     * https://docs.browserbase.com/features/proxies
+     * 
+     * @default false
+     */
+    proxies? : boolean;
+    /**
+     * Potential Browserbase Context to use 
+     * Would be a context ID 
+     */
+    context?: string;
+    /**
+     * Whether or not to port to a server
+     * 
+     */
+    server?: {
+        /**
+         * The port to listen on for SSE or MCP transport.
+         */
+        port?: number;
+        /**
+         * The host to bind the server to. Default is localhost. Use 0.0.0.0 to bind to all interfaces.
+         */
+        host?: string;
+      },
+};
---
+++ browserbase/src/program.ts
@@ -0,0 +1,42 @@
+import { program } from 'commander';
+
+import { createServer } from './index.js';
+import { ServerList } from './server.js';
+
+import { startHttpTransport, startStdioTransport } from './transport.js';
+
+import { resolveConfig } from './config.js';
+
+import packageJSON from '../package.json' with { type: 'json' };
+
+program
+    .version('Version ' + packageJSON.version)
+    .name(packageJSON.name)
+    .option('--proxies', 'Use Browserbase proxies.')
+    .option('--context <contextId>', 'Browserbase Context to use.')
+    .option('--port <port>', 'Port to listen on for SSE transport.')
+    .option('--host <host>', 'Host to bind server to. Default is localhost. Use 0.0.0.0 to bind to all interfaces.')
+    .action(async options => {
+      const config = await resolveConfig(options);
+      const serverList = new ServerList(async() => createServer(config));
+      setupExitWatchdog(serverList);
+
+      if (options.port)
+        startHttpTransport(+options.port, options.host, serverList);
+      else
+        await startStdioTransport(serverList);
+    });
+
+function setupExitWatchdog(serverList: ServerList) {
+  const handleExit = async () => {
+    setTimeout(() => process.exit(0), 15000);
+    await serverList.closeAll();
+    process.exit(0);
+  };
+
+  process.stdin.on('close', handleExit);
+  process.on('SIGINT', handleExit);
+  process.on('SIGTERM', handleExit);
+}
+
+program.parse(process.argv);
